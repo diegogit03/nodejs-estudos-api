b710c0d28fa254953a77dcda39d77840ebf43a78
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-03-03 11:45:34 -0300

    links for documentation in assertStrict.js

--- assertations/assertStrict.js
+++ assertations/assertStrict.js
@@ -7,3 +7,10 @@ assertStricted.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, '3']], 4, 5]);
 const assertNotStricted = require('assert');
 
 assertNotStricted.deepStrictEqual([[[1, 2, 3]], 4, 5], [[[1, 2, '3']], 4, 5]);
+
+/**
+ * More about:
+ *
+ * Strict Mode: https://nodejs.org/dist/latest-v14.x/docs/api/assert.html#assert_strict_assertion_mode
+ * Legacy Mode: https://nodejs.org/dist/latest-v14.x/docs/api/assert.html#assert_legacy_assertion_mode
+ */
