81f491b69c7efeba627dd8b38d3f00e82dc87fb7
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-03-04 16:23:04 -0300

    assertLike and your alias

---
+++ assertations/assertLike.js
@@ -0,0 +1,17 @@
+/**
+ * Short description
+ *
+ * Tests if value is truthy. It is equivalent to assert.equal(!!value, true, message).
+ */
+
+const assert = require('assert');
+
+assert(true, 'Asserting True');
+assert.ok(true, 'Asserting False');
+
+/**
+ * More about
+ *
+ * assert.ok(value[, message]): https://nodejs.org/dist/latest-v14.x/docs/api/assert.html#assert_assert_ok_value_message
+ * assert(value[, message]): https://nodejs.org/dist/latest-v14.x/docs/api/assert.html#assert_assert_value_message
+ */
